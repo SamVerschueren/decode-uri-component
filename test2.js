5e950ac568f9d9b6d45ded89e21ad16d2e6d2482
Martin A. Jackson <martin.a.jackson@gmail.com>
2022-11-28 21:31:38 -0600

    Create test2.js
    used to show Denial-of-Service and then test correction.

---
+++ test2.js
@@ -0,0 +1,8 @@
+
+// const decodeUriComponent = require('decode-uri-component');
+const decodeUriComponent = require('./index.js');
+
+var x = decodeUriComponent('%ea%ba%5a%ba');
+
+console.log(x);
+
